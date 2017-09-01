0485005d4a4b4efef4142f84f9d2cf2a8bb09a28
Максим Долгих <v1227873@yandex.ru>
2017-09-01 15:41:42 +0500

    Финальный этап

--- js/popup.js
+++ js/popup.js
@@ -2,13 +2,12 @@ var link = document.querySelector(".btn-about-us");
 var popup = document.querySelector(".modal-content");
 var overlay = document.querySelector(".modal-overlay");
 var close = document.querySelector(".btn-close");
-link.addEventListener("click", function(evt) {
+link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-content-show");
   overlay.classList.add("modal-overlay-show");
 });
-
-close.addEventListener("click", function(evt) {
+close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-content-show");
   overlay.classList.remove("modal-overlay-show");
